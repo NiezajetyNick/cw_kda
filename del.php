50e8d627c5673f2ad015c202fe33f22f2cecc6cc
Artur <62010277+NiezajetyNick@users.noreply.github.com>
2021-06-28 01:41:11 +0200

    Add files via upload

--- del.php
+++ del.php
@@ -8,7 +8,7 @@
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     $sql = $conn->query("INSERT INTO `cw-kda`(`K`, `D`, `A`) VALUES ('".($_POST['K']*-1)."','".($_POST['D']*-1)."','".($_POST['A']*-1)."')");
-    $sql1 = $conn->query("INSERT INTO `inTot`(`K`, `D`, `A`) VALUES ('".$_POST['K']."','".$_POST['D']."','".($_POST['A']*2)."')");
+    $sql1 = $conn->query("INSERT INTO `inTot`(`K`, `D`, `A`) VALUES ('".$_POST['K']."','".$_POST['D']."','".($_POST['A'])."')");
 
     mysqli_query($conn, $sql, $sql1);
 
